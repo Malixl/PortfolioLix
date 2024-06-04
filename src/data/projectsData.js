ab9397ff3a8c2c2758d479f8480080581fe2225e
Malik <malikmatoha19052003@gmail.com>
2024-06-04 23:33:11 +0700

    Update projectsData.js: Remove unused project entries

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -39,34 +39,6 @@ const projectsData = [
       image: "/Lix Logo.png",
       name: "Logo Design - Lix",
       details: "Curated collection of logos I've designed for competitions, academic assignments, and website branding"
-    },
-    {
-      id: 6,
-      image: "/Poster Design.png",
-      name: "Traditional Foods Poster",
-      details: "Poster design with the theme of traditional Gorontalo foods, Dumalo & Balapis",
-      projectUrl: "https://drive.google.com/drive/folders/13gs-WEjlc9OV_1OKtBy5vcGu7zE-As-E?usp=sharing",
-    },
-    {
-      id: 7,
-      image: "/Redesign Product.png",
-      name: "Redesign Product",
-      details: "Redesign packaging design of Gorontalo's MSME products to make more appealing to customers",
-      projectUrl: "https://www.figma.com/design/qJ8oCJXO9LnOo7VcDAzua1/Redesign-Packaging?node-id=0-1&t=fBbUeXoaugpQTUb0-1",
-    },
-    {
-      id: 8,
-      image: "/MerchKSL.png",
-      name: "KSL Merchandise",
-      details: "Design merchandise for Kelompok Studi Linux, including t-shirts, totebag, keychain, and hatd",
-      projectUrl: "https://drive.google.com/drive/folders/1zmy5qBAR4Dhy-9VhKNQuX2mR_bWJdFWR?usp=sharing",
-    },
-    {
-      id: 9,
-      image: "/IKIFeeds.png",
-      name: "IKI Instagram Feeds",
-      details: "Design ksl instagram feeds for event IT knowledge improvement",
-      projectUrl: "https://www.figma.com/design/selSvEqvK32wM4LeRkR7Mi/KSL-Batch-12?node-id=0-1&t=vl71MGmZmth6mkNi-1",
     }
   ];
   
