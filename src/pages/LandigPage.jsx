10a66e25269dffc0d89306346a6b553413d1024b
Malik <malikmatoha19052003@gmail.com>
2024-06-07 18:49:35 +0700

    Update landing page resume URL

--- src/pages/LandigPage.jsx
+++ src/pages/LandigPage.jsx
@@ -22,9 +22,9 @@ function LandingPage() {
               onClick={() => {
                 // Replace URL with your resume URL
                 window.location.href =
-                  "https://drive.google.com/file/d/1ZXk2NMedFSmAbjsra_QrCECT9saaq6Cq/view?usp=sharing";
+                  "https://drive.google.com/file/d/1zTUfAF4SVXL9ZrpF2kS1bjFRXjymvyws/view?usp=sharing";
               }}
-              className="bg-white hover:bg-zinc-500 text-black hover:text-white font-bold py-1 px-8 rounded "
+              className="bg-white hover:bg-zinc-500 text-black hover:text-white font-bold py-1 px-8 rounded"
             >
               cv
             </button>
